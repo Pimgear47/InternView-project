8a2ebaa0538a1d136fc5d5e1a21e150195d461d7
Kunjanaphorn <Kunjanaphorn.b@gmail.com>
2019-03-15 11:35:11 +0700

    first commit already make auth

---
+++ README.md
@@ -0,0 +1,1 @@
+"# InternView-project" 
---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+window.axios = require('axios');
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: '#app'
+});
