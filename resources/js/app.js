77063de24934407c699891103579cd251471e339
Kunjanaphorn <Kunjanaphorn.b@gmail.com>
2019-03-27 00:42:28 +0700

    edit add.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,14 +4,11 @@ window.Vue = require('vue');
 window.axios = require('axios');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('search-main', require('./components/SearchMain.vue').default);
 Vue.component('main-page', require('./components/MainPage.vue').default);
 
 Vue.component('list-page', require('./components/ListOrg/ListPage.vue').default);
-Vue.component('orgs', require('./components/ListOrg/Orgs.vue').default);
 Vue.component('edit', require('./components/ListOrg/Edit.vue').default);
 Vue.component('create', require('./components/ListOrg/Create.vue').default);
-Vue.component('search', require('./components/ListOrg/Search.vue').default);
 Vue.component('show-org', require('./components/ListOrg/ShowOrg.vue').default);
 
 const app = new Vue({
